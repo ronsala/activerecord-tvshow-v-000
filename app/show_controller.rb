afd0792e26000f3a441889053fffb48c35854f12
Ron Sala <ron.sala@gmail.com>
2018-06-13 22:11:39 +0000

    automatically backed up by learn

--- app/show_controller.rb
+++ app/show_controller.rb
@@ -1,14 +1,27 @@
-highest_rating
+def highest_rating
 #: this method should return the highest value in the ratings column. hint: if there is a minimum Active Record method, might there be a maximum method?
-most_popular_show
+end
+
+def most_popular_show
 # : this method should return the show with the highest rating. hint: use the highest_rating method as a helper method.
-lowest_rating
+end
+
+def lowest_rating
 # : returns the lowest value in the ratings column.
-least_popular_show
+end
+
+def least_popular_show
 # : returns the show with the lowest rating.
-ratings_sum
+end
+
+def ratings_sum
 # : returns the sum of all of the ratings.
-popular_shows
+end
+
+def popular_shows
 # : returns an array of all of the shows that have a rating greater than 5. hint: use the where Active Record method.
-shows_by_alphabetical_order
+end
+
+def shows_by_alphabetical_order
 # : returns an array of all of the shows sorted by alphabetical order according to their names. hint: use the order Active Record method.
+end
