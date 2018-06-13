42d4345bc2b3ae673dc83483dc8d072f0b1f528d
Ron Sala <ron.sala@gmail.com>
2018-06-13 22:34:52 +0000

    automatically backed up by learn

--- app/show_controller.rb
+++ app/show_controller.rb
@@ -1,5 +1,6 @@
 def highest_rating
 #: this method should return the highest value in the ratings column. hint: if there is a minimum Active Record method, might there be a maximum method?
+  Show.maximum("rating")
 end
 
 def most_popular_show
