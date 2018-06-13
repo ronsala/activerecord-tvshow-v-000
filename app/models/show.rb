8c04432d8192311ddb30080037de57beb45eefb1
Ron Sala <ron.sala@gmail.com>
2018-06-13 22:51:06 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -18,7 +18,7 @@ class Show < ActiveRecord::Base
 
   def self.least_popular_show
   # : returns the show with the lowest rating.
-    r = self.highest_rating
+    r = self.lowest_rating
     Show.find_by(rating: r)
   end
 
